15aa770fcc7643f5a651dc36c03a90da144ba91a
krishan190 <krishnanamdeo2001@gmail.com>
2025-10-06 07:48:07 +0530

    prefix sum problem solved

---
+++ Array/PrefixSum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ArrayCC{
+
+    public static int[] PrefixSum(int numbers[]){
+        int prefixSum[]=new int[numbers.length];
+
+        prefixSum[0]=numbers[0];
+
+        for(int i=1;i<numbers.length;i++){
+            prefixSum[i]=prefixSum[i-1]+numbers[i];
+        }
+
+        return prefixSum;
+    }
+
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+
+        System.out.println(Arrays.toString(PrefixSum(arr)));
+        System.out.println(PrefixSum(arr));
+    }
+}
