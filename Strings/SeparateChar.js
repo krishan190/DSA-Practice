87f7dcd4ff59976e8ce443ae214db9063afa9a44
krishan190 <krishnanamdeo2001@gmail.com>
2026-03-03 22:58:37 +0530

    changes in separate char file

--- Strings/SeparateChar.js
+++ Strings/SeparateChar.js
@@ -1,12 +1,14 @@
-const arr2 = ['a', 1, 'b', 2, 'c', 3]
+const arr2 = ["a", 1, "b", 2, "c", 3];
 let num = [];
 let str = [];
 for (let i = 0; i < arr2.length; i++) {
-    if (typeof (arr2[i]) === "string") {
-        str.push(arr2[i]);
-    } else {
-        num.push(arr2[i]);
-    }
+  if (typeof arr2[i] === "string") {
+    str.push(arr2[i]);
+  } else {
+    num.push(arr2[i]);
+  }
 }
-console.log(num)
-console.log(str)
+console.log(num);
+console.log(str);
+
+//separate char added in main branch
