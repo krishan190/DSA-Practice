70f27a237eaf7a01f3bc9939326a97cd0e0d1316
krishan190 <krishnanamdeo2001@gmail.com>
2026-03-10 18:29:01 +0530

    merge sorted array

--- Array/Basic Array/MergeTwoSorted.js
+++ Array/Basic Array/MergeTwoSorted.js
@@ -9,18 +9,19 @@
 */
 
 function mergeSorted(arr1, arr2) {
-    let result = [];
-    let i = 0, j = 0;
+  let result = [];
+  let i = 0,
+    j = 0;
 
-    while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
-            result.push(arr1[i++]);
-        } else {
-            result.push(arr2[j++]);
-        }
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i++]);
+    } else {
+      result.push(arr2[j++]);
     }
+  }
 
-    return result.concat(arr1.slice(i).concat(arr2.slice(j)));
+  return result.concat(arr1.slice(i).concat(arr2.slice(j)));
 }
 
-console.log(mergeSorted([1, 3, 5], [2, 4, 6]));
+console.log(mergeSorted([1, 2, 3], [2, 5, 6]));
