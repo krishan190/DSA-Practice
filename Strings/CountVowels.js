edfd1cf8fcb9223b1c8757872db6c059869fac51
krishan190 <krishnanamdeo2001@gmail.com>
2026-04-02 23:35:28 +0530

    vowel and consonent solved

--- Strings/CountVowels.js
+++ Strings/CountVowels.js
@@ -1,12 +1,26 @@
-const str = "The happiest place in earth";
-const vowels = ['a', 'e', 'i', 'o', 'u'];
+const str = "krishan";
+const vowels = ["a", "e", "i", "o", "u"];
 
 function countVowels(str) {
-    let count = 0;
-    str.toLowerCase().split("").forEach(char => {
-        vowels.includes(char) ? count++ : null
+  let vowelCount = 0;
+  let consonentCount = 0;
+
+  str
+    .toLowerCase()
+    .split("")
+    .forEach((char) => {
+      if (char >= "a" && char <= "z") {
+        if (vowels.includes(char)) {
+          vowelCount++;
+        } else {
+          consonentCount++;
+        }
+      }
     });
-    return count;
+  return {
+    vowelCount,
+    consonentCount,
+  };
 }
 
 console.log(countVowels(str));
@@ -14,11 +28,11 @@ console.log(countVowels(str));
 // we use below instead of var we use let it gives reference error or also we use IIFE function also
 
 function show() {
-    {
-        var a = 10;
-        var b = 20;
-    }
-    console.log(`A: ${a},B:${b}`);
+  {
+    var a = 10;
+    var b = 20;
+  }
+  console.log(`A: ${a},B:${b}`);
 }
 
 show();
